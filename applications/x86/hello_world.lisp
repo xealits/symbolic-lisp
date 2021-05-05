bf33ef7fecaa7a324ae9d139dbe2cbdb254b5f98
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2021-05-05 03:43:12 +0200

    Up hello world and added it to the makefile
    	modified:   applications/x86/hello_world.lisp
    	modified:   applications/x86/makefile

--- applications/x86/hello_world.lisp
+++ applications/x86/hello_world.lisp
@@ -1,5 +1,18 @@
+(source root_env "x86.lisp")
 
 (quote (
+(static_variable "db" "message"
+    (list
+	  (join (str) (list double_quote "Hello, world!" double_quote))
+	  10
+	  )
+	)
+
+(print "message:" ".byte"
+	  (join (str) (list double_quote "Hello, world!" double_quote ","))
+	  10
+	)
+
 global _start
 section .data
 message: db 'hello, world!', 10
@@ -11,42 +24,25 @@ mov     rdi, 1           ; argument #1 in rdi: where to write (descriptor)?
 mov     rsi, message     ; argument #2 in rsi: where does the string start?
 mov     rdx, 14          ; argument #3 in rdx: how many bytes to write?
 syscall                  ; this instruction invokes a system call
-
-(.ascii 'Hello,_world!\n\0)
-
-x86_64: https://filippo.io/linux-syscall-table/
-1	write	sys_write	fs/read_write.c
-%rdi	              %rsi	            %rdx
-unsigned int fd	const char __user * buf	size_t count
-and
-use syscall instruction
-
-x86: https://www.informatik.htw-dresden.de/~beck/ASM/syscall_list.html
-%eax	Name	    Source 	            %ebx	        %ecx	        %edx	%esx	%edi
-4	    sys_write	fs/read_write.c 	unsigned int	const char *	size_t	-       -
-and
-use int 0x80 instruction
 ))
 
 
-(quote (the following is x86 assembly for hello world))
-
-(source 'x86.lisp)
-
 (.data)
-(static_variable '.ascii 'message '"Hello,_world!\n\0")
+(static_variable ".ascii" "message"
+	  (join (str) (list double_quote "Hello, world!\n\0" double_quote))
+	)
 
 (.text)
-(.globl '_start)
+(.globl "_start")
+(label "_start")
 
-(label '_start)
+(mov "$1" rax)
+(mov "$1" rdi)
+(mov "$message" rsi)
+(mov "$14" rdx)
+(syscall)
 
-(movl '$4       eax) (quote (syscall ID write))
-(movl '$1       ebx) (quote (file descriptor = 1, stdout ))
-(movl '$message ecx)
-(movl '$14      edx)
-(interrupt_to KERNEL)
-
-(movl '$1       eax) (quote (syscall ID exit))
-(movl '$0       ebx) (quote (exit code))
-(interrupt_to KERNEL)
+(quote (syscall exit process))
+(mov "$60"     rax)
+(mov "$0"      rdi)
+(syscall)
